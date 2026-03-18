b45b6cd5f2a6674dd4c1225816b3079e60647055
Oscar <olariosm@outlook.com>
2026-03-17 18:51:37 -0600

    cambio

--- Clase-Mar-17/main.py
+++ Clase-Mar-17/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, Field
 from uuid import uuid4, UUID
@@ -134,3 +135,18 @@ async def eliminar_solicitud(id: UUID):
 
     del solicitudes[id]
     return None
+
+
+origins = ["*"] # Lista de orígenes permitidos para CORS, en este caso se permite cualquier origen. En un entorno de producción, es indispensable especificar los orígenes permitidos para mejorar la seguridad.
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenido a la API de mascotas!"}
